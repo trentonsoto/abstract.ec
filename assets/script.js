214524de19e9213f5a069a3221f1835038234695
trentonsoto <sotot946@newschool.edu>
2026-05-15 23:51:08 -0400

    fixed the active state

--- assets/script.js
+++ assets/script.js
@@ -7,10 +7,14 @@ dark.addEventListener('click', () => {
     document.body.classList.add('dark-mode');
     document.body.classList.add('dark-mode');
     document.body.classList.remove('light-mode');
+    light.classList.remove('active');
+    dark.classList.add('active');
 })
 
 light.addEventListener('click', () => {
     document.body.classList.remove('dark-mode');
     document.body.classList.add('light-mode');
     document.body.classList.remove('dark-mode');
+    light.classList.add('active');
+    dark.classList.remove('active');
 })
